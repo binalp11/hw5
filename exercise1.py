e4776bcb004bc78411c38ff7d0df34f591239369
Binal Patel <binalp11@gmail.com>
2015-02-03 03:07:59 +0000

    Made a few changes to make output clear

--- exercise1.py
+++ exercise1.py
@@ -3,17 +3,21 @@
 #opens the input text into a object
 dna = open('input.txt')
 
+#opens shortseq.txt with write permissions and is called shortseq
+shortseq = open('shortseq.txt', 'w')
+
+
 #each item in dna will run through the for loop 
 for ii in dna:
 
     #removes the first 14 characters of each line
     withoutseqadapt = ii[14:]
 
+
     #sends new sequence to the output file named shortseq
     shortseq.write(withoutseqadapt)
 
-    #prints the sequence length for each
-    print("sequence length is " + str(len(withoutseqadapt)))
+    #prints the sequence and the length for each
+    print(withoutseqadapt.rstrip('\n'))
+    print("sequence length is " + str(len(withoutseqadapt)) + '\n')
 
- #close file
- shortseq.close()   
